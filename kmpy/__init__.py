2f8f5bfae7412ca8f2286dde894c02894b43866e
Deren <de2356@columbia.edu>
2020-12-27 17:57:06 -0500

    add init

---
+++ kmpy/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+"""
+Primary classes of Kmpy (Kmer-python)
+"""
+
+__version__ = "0.0.1"
+
+
+from kmpy.kcount import Kcount
+from kmpy.kgroup import Kgroup
+from kmpy.kfilter import Kfilter
+from kmpy.utils import Logger as _Logger
+
+_Logger()
