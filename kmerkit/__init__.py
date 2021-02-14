d4b0d1def9623ffc81f7033ed2fc22ec5c2d4b9e
Deren <de2356@columbia.edu>
2021-02-14 18:24:25 -0500

    devel version up

--- kmerkit/__init__.py
+++ kmerkit/__init__.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
 """
-Primary classes of Kmpy (Kmer-python)
+Primary classes of kmerkit
 
 TODO: A tool to grab the largest file and kcount as cs255 and cs655
 to test for whether we should use uint8 versus uint16. 
 Why not just handle this completely for the user to avoid problems.
 """
 
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 
 from loguru import logger
 from kmerkit.kcount import Kcount
