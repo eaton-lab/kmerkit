53aeb91bbcaba7ac7f8c23c1eb45285039490b86
Deren <de2356@columbia.edu>
2021-04-27 10:28:04 -0400

    w/ branch

--- kmerkit/__init__.py
+++ kmerkit/__init__.py
@@ -8,7 +8,7 @@ to test for whether we should use uint8 versus uint16.
 Why not just handle this completely for the user to avoid problems.
 """
 
-__version__ = "0.0.14"
+__version__ = "0.0.16"
 
 from loguru import logger
 
